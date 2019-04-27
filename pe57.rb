c83b73a26ebc54cf6e09025f992e4e6868b2c95d
Joseph Campbell <1campbellj@gmail.com>
2019-04-26 22:22:31 -0500

    Solve 57

---
+++ pe57.rb
@@ -0,0 +1,15 @@
+def sqrt_2(n)
+  return 1 if n == 0
+
+  1 + (1 / (1 + sqrt_2(n - 1).to_r).to_r)
+end
+
+sum = 0
+for i in 1..1_000
+  r = sqrt_2(i)
+  if r.numerator.to_s.length > r.denominator.to_s.length
+    sum += 1
+  end
+end
+
+puts sum
